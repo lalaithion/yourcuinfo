b887812bed94a8732dafb871a4108bd74830b6db
Izaak Weiss <izwe5504@colorado.edu>
2017-03-02 17:43:36 -0700

    Parser for catalog finished

--- src/catalog/parser.py
+++ src/catalog/parser.py
@@ -1,9 +1,82 @@
+from html.parser import HTMLParser
+from string import printable
+from time import strftime, gmtime
+import json
+from os import listdir
+from os.path import isfile, join
+
+log = open("parse.log", 'a+')
+errors = False
+
+def format_title(inp):
+    ls = inp.split(' ')
+    label = ls[0]
+    title = ' '.join(ls[2:])
+    ls = label.split('-')
+    dept = ls[0]
+    number = ls[1]
+    return dept, number, title
+
+class Course():
+    def __init__(self,data):
+        self.data = data
+    def __getattr__(self,name):
+        return self.data[name]
+    def __repr__(self):
+        rep = "Class:" + self.type + "," + self.time + ")"
+        return rep
+    def json(self):
+        return self.data
+
+
+class MyHTMLParser(HTMLParser):
+    def __init__(self,verbose = True):
+        self.courses = []
+        self.fields = {}
+        self.current = None
+        self.title = None
+        self.verbose = verbose
+        super(MyHTMLParser, self).__init__()
+
+    def handle_starttag(self, tag, attrs):
+        clss = dict(attrs).get("class", None)
+        if clss == "node-title":
+            self.current = "title"
+        if clss == "field-items":
+            self.current = "body"
+
+    def handle_endtag(self, tag):
+        # at end of class
+        if self.current == "body":
+            if self.verbose:
+                print("Class: " + self.fields["title"])
+                print("Description: " + self.fields["body"])
+                print()
+            dept, number, title = format_title(self.fields["title"])
+            self.fields["title"] = title
+            self.fields["number"] = number
+            self.fields["dept"] = dept
+            self.courses.append(Course(self.fields))
+            self.fields = {}
+            self.current = None
+        if self.current in self.fields:
+            self.current = None
+
+    def handle_data(self, data):
+        if self.current is not None:
+            printable_data = "".join(filter(lambda x: x in printable, data))
+            self.fields[self.current] = printable_data
+
+    def json(self):
+        return {i.dept + i.number:i.json() for i in self.courses}
+
 def jsonify(filename):
     assert(filename.endswith(".html"))
     with open(filename, "r") as f:
-        parser = MyHTMLParser(False)
+        parser = MyHTMLParser()
         parser.feed(f.read())
-        return parser.json()
+        j = parser.json()
+    return j
 
 def jsonify_dir(dirpath):
     class_info = []
@@ -13,13 +86,15 @@ def jsonify_dir(dirpath):
             if filepath.endswith(".html"):
                 print("Reading file:", filepath)
                 try:
+                    print("Hello world")
                     info = jsonify(filepath)
+                    print("Goodbye world")
                     if not info:
                         raise Exception("Unable to parse file")
                     class_info.append(info)
                 except Exception as err:
                     errors = True
-                    log.write("Error during parsing of {0}:\n  {1}\n".format(filepath, err))
+                    log.write("Error during parsing of {0}:\n  {1}\n".format(filepath, str(err)))
                     
         else:
             print("Reading dir:", filepath)
@@ -28,17 +103,15 @@ def jsonify_dir(dirpath):
 
 def main():
     date = strftime("%Y-%m-%d", gmtime())
-    log = open("parse.log", 'a+')
     log.write("{0}\n{1}: Beginning parse:\n".format(date, strftime("%H:%M", gmtime())))
     errors = False
 
     root = "../catalog_html/" + date + "/"
     catalog = jsonify_dir(root)
     with open('../json/catalog.json', 'w') as outfile:
-        json.dump(catalog, outfile)
+        json.dump(catalog, outfile, indent=4, separators=(',', ': '))
         
     log.write("{0}: Parse finished\n".format(strftime("%H:%M", gmtime())))
-    log.close()
     print("Parse finished with no errors" if not errors else "Parse finished with errors")
 
 if __name__ == "__main__":
