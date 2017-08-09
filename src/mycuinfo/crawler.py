695fcb8e735e9326fad686985d9af36a52455a10
Alexander Curtiss <apccurtiss@gmail.com>
2017-08-08 23:01:05 -0600

    Got multithreaded crawler and parser working with the recent myCUinfo update.

--- src/mycuinfo/crawler.py
+++ src/mycuinfo/crawler.py
@@ -9,45 +9,51 @@ import time
 import os
 import errno
 import threading
+import queue
+import logging
 
-url = "https://portal.prod.cu.edu/psp/epprod/UCB2/ENTP/h/?tab=DEFAULT" # mycuinfo url
+logFormatter = logging.Formatter('%(asctime)s %(message)s', '%H:%M:%S')
+root_logger = logging.getLogger()
+root_logger.setLevel(logging.INFO)
+
+handlers = [
+    logging.FileHandler('../../docs/logs/mycuinfo.log'),
+    logging.StreamHandler(),
+]
+
+for handler in handlers:
+    handler.setFormatter(logFormatter)
+    root_logger.addHandler(handler)
+
+url = 'https://portal.prod.cu.edu/psp/epprod/UCB2/ENTP/h/?tab=DEFAULT' # mycuinfo url
 login_timer = 20 # time for login to complete
 expand_timer = 10 # time for dropdown arrows to open
-print_debug = False
 
 def retry(f, *pargs, max_seconds=30, **kwargs):
     for i in range(max_seconds):
         try:
             return f(*pargs, **kwargs)
         except Exception:
-            sleep(1)
+            time.sleep(1)
     return f(*pargs, **kwargs)
 
-def wait_until_fails(f, *pargs, max_seconds=15, **kwargs):
-    try:
-        for i in range(max_seconds):
-            f(*pargs, **kwargs)
-            sleep(1)
-    except Exception:
-        return True
-    return False
-
-def debug(m):
-    if print_debug:
-        print(m)
-
-def wait_for_loading_icon():
-    debug('Waiting for loading icon')
-    if not wait_until_fails(deiver.find_elements_by_class_name, 'ui-icon-loading'):
-        raise Exception("Loading icon did not vanish")
+def wait_for_loading_icon(driver, max_seconds=5):
+    SLEEP_INTERVAL = 0.5
+    loading_icon = driver.find_elements_by_class_name('ui-icon-loading')[0]
+    for i in range(int(max_seconds / SLEEP_INTERVAL)):
+        if not loading_icon.is_displayed():
+            return
+        time.sleep(SLEEP_INTERVAL)
+    raise Exception('Loading icon failed to disappear!')
 
 def login(ukeys, pkeys):
     driver = webdriver.Chrome()
     driver.get(url)
-    username = driver.find_element_by_id("username")
+    logging.debug('Logging in')
+    username = driver.find_element_by_id('username')
     username.clear()
     username.send_keys(ukeys)
-    passwd = driver.find_element_by_id("password")
+    passwd = driver.find_element_by_id('password')
     passwd.clear()
     passwd.send_keys(pkeys)
     passwd.send_keys(Keys.RETURN)
@@ -55,89 +61,86 @@ def login(ukeys, pkeys):
 
 
 def runSearch(driver, current, second_time=False):
-    debug('Clicking "search for classes"')
-    search = retry(driver.find_element_by_link_text, 'Search for Classes').click()
+    logging.debug('Clicking "search for classes"')
+    search = retry(driver.find_element_by_link_text, 'Search for Classes')
+    retry(search.click)
 
-    debug('Switching to main frame')
-    retry(driver.switch_to_frame, "ptifrmtgtframe")
+    logging.debug('Switching to main frame')
+    retry(driver.switch_to_frame, 'ptifrmtgtframe')
 
-    debug('Selecting campus')
+    logging.debug('Selecting institution')
     Select(
-        retry(driver.find_element_by_id, "CLASS_SRCH_WRK2_INSTITUTION$31$")
-    ).select_by_value("CUBLD")
+        driver.find_element_by_id('CLASS_SRCH_WRK2_INSTITUTION$31$')
+    ).select_by_value('CUBLD')
 
-    wait_for_loading_icon()
+    wait_for_loading_icon(driver)
 
-    debug('Selecting semester')
+    logging.debug('Selecting semester')
     Select(
-        retry(driver.find_element_by_id, "CLASS_SRCH_WRK2_STRM$35$")
+        driver.find_element_by_id('CLASS_SRCH_WRK2_STRM$35$')
     ).select_by_visible_text('Fall 2017 UC Boulder')
 
-    wait_for_loading_icon()
+    wait_for_loading_icon(driver)
 
-    debug('Selecting campus')
+    logging.debug('Selecting campus')
     Select(
-        retry(driver.find_element_by_id, "SSR_CLSRCH_WRK_CAMPUS$0")
+        driver.find_element_by_id('SSR_CLSRCH_WRK_CAMPUS$0')
     ).select_by_visible_text('Boulder Main Campus')
 
-    wait_for_loading_icon()
+    wait_for_loading_icon(driver)
 
-    debug('Entering department')
-    find_elem(driver.find_element_by_id,"SSR_CLSRCH_WRK_SUBJECT$1").send_keys(current)
+    logging.debug('Entering department')
+    driver.find_element_by_id('SSR_CLSRCH_WRK_SUBJECT$1').send_keys(current)
 
     # Chem has too many classes! AAAAAH
-    if current == "CHEM":
-        debug('Splitting CHEM into two groups')
+    if current == 'CHEM':
+        logging.debug('Splitting CHEM into two groups')
         if second_time:
             Select(
-                retry(driver.find_element_by_id, "SSR_CLSRCH_WRK_SSR_EXACT_MATCH1$2")
-            ).select_by_visible_text("greater than or equal to")
+                retry(driver.find_element_by_id, 'SSR_CLSRCH_WRK_SSR_EXACT_MATCH1$2')
+            ).select_by_visible_text('greater than or equal to')
         else:
             Select(
-                retry(driver.find_element_by_id, "SSR_CLSRCH_WRK_SSR_EXACT_MATCH1$2")
-            ).select_by_visible_text("less than or equal to")
+                retry(driver.find_element_by_id, 'SSR_CLSRCH_WRK_SSR_EXACT_MATCH1$2')
+            ).select_by_visible_text('less than or equal to')
 
-        wait_for_loading_icon()
+        wait_for_loading_icon(driver)
 
-        retry(driver.find_element_by_id, "SSR_CLSRCH_WRK_CATALOG_NBR$2").send_keys("3000")
+        retry(driver.find_element_by_id, 'SSR_CLSRCH_WRK_CATALOG_NBR$2').send_keys('3000')
 
-    wait_for_loading_icon()
+    wait_for_loading_icon(driver)
 
-    debug('Submitting')
-    retry(driver.find_elements_by_class_name, "gh-footer-item")[1].click()
+    logging.debug('Submitting')
+    retry(driver.find_elements_by_class_name, 'gh-footer-item')[1].click()
 
-
-def department(filepath, current, log, login_data, second_time=False):
-    print(current)
+def scrape_department(filepath, current, login_data, second_time=False):
     try:
-        driver = login(login_data["uname"], login_data["pswd"])
+        driver = login(login_data['uname'], login_data['pswd'])
     except Exception as err:
-        log.write("Error logging in as {0}:\n  {1}\n".format(login_data["uname"], err))
+        logging.info('Error logging in as %d' % (login_data['uname'], err))
         driver.close()
         raise err
-
-    # time.sleep(login_timer)
 
     try:
         runSearch(driver, current, second_time)
     except Exception as err:
-        log.write("Error getting to classes {0}:\n  {1}\n".format(current, err))
+        logging.info('Error getting to classes {0}:\n  {1}\n'.format(current, err))
         driver.close()
         raise err
 
     time.sleep(1)
 
     try:
-        driver.find_element_by_id("win0divDERIVED_CLSMSG_ERROR_TEXT")
+        driver.find_element_by_id('win0divDERIVED_CLSMSG_ERROR_TEXT')
         return
     except:
         pass
 
 
     try:
-        find_elem(driver.find_element_by_id, "CU_CLS_RSL_WRK_CU_SSR_EXPAND_ALL").click()
+        retry(driver.find_element_by_id, 'CU_CLS_RSL_WRK_CU_SSR_EXPAND_ALL').click()
     except Exception as err:
-        log.write("Error exapnding classes {0}:\n  {1}\n".format(current, err))
+        logging.info('Error exapnding classes {0}:\n  {1}\n'.format(current, err))
         driver.close()
         raise err
 
@@ -146,78 +149,56 @@ def department(filepath, current, log, login_data, second_time=False):
     try:
         if second_time:
             current = current + '2'
-        with open(filepath + current + ".html", 'w') as f:
+        with open(filepath + current + '.html', 'w') as f:
             f.write(driver.page_source)
             f.close()
     except Exception as err:
-        log.write("Error saving {0} to file:\n  {1}\n".format(current, err))
+        logging.info('Error saving {0} to file:\n  {1}\n'.format(current, err))
         driver.close()
         raise err
 
     driver.close()
 
-    if current == "CHEM" and not second_time:
-        department(filepath, current, log, login_data, True)
+    if current == 'CHEM' and not second_time:
+        scrape_department(filepath, current, login_data, True)
 
-def redo(depts):
-        ukeys = input("User: ").strip('\n')
-        pkeys = getpass.getpass()
-        login = {"uname": ukeys, "pswd": pkeys}
-
-        filepath = "../mycuinfo_html/"
-
-        date = time.strftime("%Y-%m-%d", time.gmtime())
-        log = open(filepath + "scrape.log", 'w+')
-        log.write("{0}\n{1}: Beggining new data harvest\n".format(date, time.strftime("%H:%M", time.gmtime())))
-
-        error = False
-        for current in depts:
-            import os.path
-            if os.path.isfile(filepath + current + '.html'):
-                continue
-            try:
-                department(filepath, current, log, login)
-            except Exception as err:
-                error = True
-                continue
-
-        log.write("{0}: Finished data harvest\n".format(time.strftime("%H:%M", time.gmtime())))
-        log.close()
-
-        if error:
-            print("Scrape finished with Errors")
-        else:
-            print("!!! Scrape finished with Errors !!!")
-
-def main(depts, n_threads=15, logpath="../../docs/logs/mycuinfo.log", filepath="../../docs/raw_html/mycuinfo/"):
-    ukeys = input("User: ").strip('\n')
+def main(depts, n_threads=10, filepath='../../docs/raw_html/mycuinfo/'):
+    logging.info('Beggining new data harvest')
+    ukeys = input('User: ').strip('\n')
     pkeys = getpass.getpass()
-    login = {"uname": ukeys, "pswd": pkeys}
+    login = {'uname': ukeys, 'pswd': pkeys}
 
-    start_date = time.strftime("%Y-%m-%d", time.gmtime())
-    log = open(logpath, 'w')
-    log.write("{0}\n{1}: Beggining new data harvest\n".format(start_date, time.strftime("%H:%M", time.gmtime())))
+    departments = queue.Queue()
+    for dept in depts:
+        departments.put(dept)
 
-    error = False
-    for current in depts:
-        try:
-            department(filepath, current, log, login)
-        except Exception as err:
-            raise err
-            error = True
-            continue
+    threads = []
+    for i in range(n_threads):
+        threads.append(QueuedThread(departments, filepath, login))
+        threads[i].start()
 
-    log.write("{0}: Finished data harvest\n".format(time.strftime("%H:%M", time.gmtime())))
-    log.close()
+    for i in range(n_threads):
+        threads[i].join()
 
-    if not error:
-        print("Scrape finished with no Errors")
-    else:
-        print("!!! Scrape finished with Errors !!!")
+    logging.info('Finished data harvest')
 
-class ThreadedWebInstance(object):
-    def run():
-        pass
+class QueuedThread(threading.Thread):
+    def __init__(self, queue, filepath, login):
+        super(QueuedThread, self).__init__()
+        self.queue = queue
+        self.login = login
+        self.filepath = filepath
+        self.results = []
 
-if __name__ == "__main__":
-    main()
+    def run(self):
+        while not self.queue.empty():
+            try:
+                dept = self.queue.get_nowait()
+                self.results.append(scrape_department(self.filepath, dept, self.login))
+                self.queue.task_done()
+            except queue.Empty:
+                continue
+
+if __name__ == '__main__':
+    from departments_list import departments
+    main(departments)
--- src/mycuinfo/parser.py
+++ src/mycuinfo/parser.py
@@ -1,11 +1,26 @@
 #!/usr/local/bin/python3
 
+import json
+import logging
+import re
 from html.parser import HTMLParser
 from string import printable
 from time import strftime, gmtime
-import json
 from os import listdir
 from os.path import isfile, join
+
+logFormatter = logging.Formatter('%(asctime)s %(message)s', '%H:%M:%S')
+root_logger = logging.getLogger()
+root_logger.setLevel(logging.DEBUG)
+
+handlers = [
+    # logging.FileHandler('../../docs/logs/mycuinfo.log'),
+    logging.StreamHandler(),
+]
+
+for handler in handlers:
+    handler.setFormatter(logFormatter)
+    root_logger.addHandler(handler)
 
 class Section():
     def __init__(self,data):
@@ -66,90 +81,87 @@ class Course():
         rep += ", sections:" + str(num_sec)
         rep += ")"
         return rep
+
     def json(self):
         json = {
             "name":self.name,
             "sections":{
-                k:[i.json() for i in v] for k,v in self.sections.items()
+                k: [i.json() for i in v] for k,v in self.sections.items()
             }
         }
         return json
 
-class MyHTMLParser(HTMLParser):
-    def __init__(self,verbose = True):
+class MyCUInfoHTMLParser(HTMLParser):
+    def __init__(self):
         self.courses = []
         self.fields = {}
-        self.current = None
+        self.current_state = None
         self.title = None
-        self.verbose = verbose
-        super(MyHTMLParser, self).__init__()
+        super(MyCUInfoHTMLParser, self).__init__()
 
     def handle_starttag(self, tag, attrs):
         for attr, value in attrs:
             if attr == "id":
-                if "win0divSSR_CLSRSLT_WRK_GROUPBOX2GP$" in value:
-                    self.current = "title"
-                    # print("On TITLE: %s" % value)
-                if "MTG_CLASS_NBR$" in value:
-                    self.current = "number"
-                if "MTG_CLASSNAME$" in value:
-                    self.current = "section"
-                if "MTG_DAYTIME$" in value:
-                    self.current = "time"
-                if "MTG_ROOM$" in value:
-                    self.current = "room"
-                if "MTG_INSTR$" in value:
-                    self.current = "instructor"
-                if "MTG_TOPIC$" in value:
-                    self.current = "dates"
-                if "CU_CLS_RSL_WRK_CU_SSR_UNITS_RANGE$" in value:
-                    self.current = "units"
-                if "CU_CLS_RSL_WRK_CU_SSR_ENRL_RES$" in value:
-                    self.current = "restriction"
-                if "CU_CLS_RSL_WRK_CU_SSR_CNSNT_REQ$" in value:
-                    self.current = "consent"
-                if "CU_CLS_RSL_WRK_AVAILABLE_SEATS$" in value:
-                    self.current = "seats"
-                if "CU_CLS_RSL_WRK_WAIT_TOT$" in value:
-                    # print("ON WAIT TOT: %s" % value)
-                    self.current = "waitlist"
-                if "CU_CLS_RSL_WRK_CU_SSR_WAITLIST$" in value:
-                    # print("On WAITLIST: %s" % value)
-                    self.current = "waitlist"
+                if re.search("win0divSSR_CLSRSLT_WRK_GROUPBOX2GP", value):
+                    self.current_state = "title"
+                if re.search("MTG_CLASS_NBR", value):
+                    self.current_state = "number"
+                if re.search("MTG_CLASSNAME", value):
+                    self.current_state = "section"
+                if re.search("MTG_DAYTIME", value):
+                    self.current_state = "time"
+                if re.search("MTG_ROOM", value):
+                    self.current_state = "room"
+                if re.search("MTG_INSTR", value):
+                    self.current_state = "instructor"
+                if re.search("MTG_TOPIC", value):
+                    self.current_state = "dates"
+                if re.search("CU_CLS_RSL_WRK_CU_SSR_UNITS_RANGE", value):
+                    self.current_state = "units"
+                if re.search("CU_CLS_RSL_WRK_CU_SSR_ENRL_RES", value):
+                    self.current_state = "restriction"
+                if re.search("CU_CLS_RSL_WRK_CU_SSR_CNSNT_REQ", value):
+                    self.current_state = "consent"
+                if re.search("CU_CLS_RSL_WRK_AVAILABLE_SEATS", value):
+                    self.current_state = "seats"
+                if re.search("^CU_CLS_RSL_WRK_WAIT_TOT", value):
+                    self.current_state = "waitlist"
+                if re.search("^CU_CLS_RSL_WRK_CU_SSR_WAITLIST", value):
+                    self.current_state = "waitlist"
+                if re.search("DERIVED_CLSRCH_DESCRLONG", value):
+                    self.current_state = "description"
 
     def handle_endtag(self, tag):
-        # at end of class
-        if self.current == "waitlist":
-            if self.verbose:
-                if "title" in self.fields:
-                    print(self.fields["title"] + ":")
-                print("Class: " + self.fields["number"])
-                print("Section: " + self.fields["section"])
-                print("Time: " + self.fields["time"])
-                print("Room: " + self.fields["room"])
-                print("Instructor: " + self.fields["instructor"])
-                print("Dates: " + self.fields["dates"])
-                #print("Status: " + self.fields["status"])
-                print("Units: " + self.fields["units"])
-                print("Enrollment Restriction: " + self.fields["restriction"])
-                print("Instructor Consent Required: " + self.fields["consent"])
-                print("Available Seats: " + self.fields["seats"])
-                print("Wait List Total: " + self.fields["waitlist"])
-                print()
-            print("Adding %s" % self.fields['title'])
+        # the waitlist is the last tag in the class
+        if self.current_state == "waitlist":
+            # logging.debug("Title: " + self.fields["title"])
+            # logging.debug("Class: " + self.fields["number"])
+            # logging.debug("Section: " + self.fields["section"])
+            # logging.debug("Time: " + self.fields["time"])
+            # logging.debug("Room: " + self.fields["room"])
+            # logging.debug("Instructor: " + self.fields["instructor"])
+            # logging.debug("Dates: " + self.fields["dates"])
+            # #logging.debug("Status: " + self.fields["status"])
+            # logging.debug("Units: " + self.fields["units"])
+            # logging.debug("Enrollment Restriction: " + self.fields["restriction"])
+            # logging.debug("Instructor Consent Required: " + self.fields["consent"])
+            # logging.debug("Available Seats: " + self.fields["seats"])
+            # logging.debug("Wait List Total: " + self.fields["waitlist"])
+            # logging.debug("Description: " + self.fields["description"])
+            # logging.debug("Adding %s" % self.fields['title'])
             self.courses[-1].add_section(self.fields)
             self.fields = {}
-            self.current = None
-        if self.current in self.fields:
-            self.current = None
+            self.current_state = None
+        if self.current_state in self.fields:
+            self.current_state = None
 
     def handle_data(self, data):
-        if self.current is not None:
+        if self.current_state == "title":
+            self.courses.append(Course(printable_data))
+        elif self.current_state is not None:
             # Filter out non-printable characters (note: find out why there are non-printable characters!?)
             printable_data = "".join(filter(lambda x: x in printable, data))
-            self.fields[self.current] = printable_data
-            if self.current == "title":
-                self.courses.append(Course(printable_data))
+            self.fields[self.current_state] = printable_data
 
     def json(self):
         return {i.identifier:i.json() for i in self.courses}
@@ -158,43 +170,42 @@ class MyHTMLParser(HTMLParser):
 def jsonify(filename):
     assert(filename.endswith(".html"))
     with open(filename, "r") as f:
-        parser = MyHTMLParser(False)
+        logging.info("Parsing %s" % filename)
+        parser = MyCUInfoHTMLParser()
         parser.feed(f.read())
         return parser.json()
 
-def jsonify_dir(dirpath, log):
+def jsonify_dir(dirpath):
     class_info = []
     for f in listdir(dirpath):
         filepath = join(dirpath, f)
         if isfile(filepath):
             if filepath.endswith(".html"):
-                print("Reading file:", filepath)
                 try:
                     info = jsonify(filepath)
                     if not info:
                         raise Exception("Unable to parse file")
                     class_info.append(info)
                 except Exception as err:
+                    raise
                     errors = True
-                    log.write("Error during parsing of %s:\n  %s: %s\n" % (filepath, type(err), err))
+                    logging.info("Error during parsing of %s:\n  %s: %s\n" % (filepath, type(err), err))
 
         else:
             print("Reading dir:", filepath)
             class_info.extend(jsonify_dir(filepath))
     return class_info
 
-def main(logpath="../../docs/logs/mycuinfo-parse.log", inpath="../../docs/raw_html/mycuinfo/", outpath='../../docs/json/classes.json'):
-    log = open(logpath, 'w')
+def main(inpath="../../docs/raw_html/mycuinfo/", outpath='../../docs/json/classes.json'):
     date = strftime("%Y-%m-%d", gmtime())
-    log.write("{0}\n{1}: Beginning parse:\n".format(date, strftime("%H:%M", gmtime())))
+    logging.info("Beginning parse")
     errors = False
 
-    classes = jsonify_dir(inpath, log)
+    classes = jsonify_dir(inpath)
     with open(outpath, 'w') as outfile:
         json.dump(classes, outfile, indent=4, separators=(',', ': '))
 
-    log.write("%s: Parse finished\n" % strftime("%H:%M", gmtime()))
-    log.close()
+    logging.info("Parse finished")
     print("Parse finished with no errors" if not errors else "Parse finished with errors")
 
 if __name__ == "__main__":
